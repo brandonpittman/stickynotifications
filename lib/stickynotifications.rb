077c9930a59a6611965912cbe394a6d043da4eb2
brandonpittman <brandonpittman@gmail.com>
2014-08-21 14:23:33 +0900

    Add documentation

--- lib/stickynotifications.rb
+++ lib/stickynotifications.rb
@@ -2,13 +2,22 @@ require "stickynotifications/version"
 require 'uri'
 require 'thor'
 
+# @author "Brandon Pittman"
+# All logic for StickyNotifications
 class StickyNotifications < Thor
+
   desc "new TEXT", "create new Sticky Notification with TEXT"
+  # Creates a new Sticky Notification
+  # @param sticky_text [String] the text of the Sticky Notification
   def new(sticky_text)
     %x{open "sticky-notifications://note?message=#{escape_string(sticky_text)}"}
   end
 
   no_commands do
+    # Escapes the Sticky Notification text for URLs. URI produces %20 for
+    # spaces instead of CGI's +.
+    # @param string_to_escape [String] the text to be escaped
+    # @return [String] parsed URL
     def escape_string(string_to_escape)
       URI.escape(string_to_escape)
     end
--- lib/stickynotifications/version.rb
+++ lib/stickynotifications/version.rb
@@ -1,3 +1,5 @@
+# Sets version for RubyGems
 module Stickynotifications
+  # Sets version for RubyGems
   VERSION = "0.0.3"
 end
